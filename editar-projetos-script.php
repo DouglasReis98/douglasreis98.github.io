5aaa2ae5507c8f2778fc4835ba2c0341b4c4a2db
Douglas Reis <68308882+DouglasReis98@users.noreply.github.com>
2022-01-25 12:51:42 -0300

    Add files via upload

--- editar-projetos-script.php
+++ editar-projetos-script.php
@@ -32,7 +32,27 @@ if (!empty($_REQUEST['action'])){
         pg_close($conn);
     }
 }
+else {
+    $pessoa = [];
+    $pessoa['id']        = '';
+    $pessoa['nome']      = '';
+    $pessoa['endereco']  = '';
+    $pessoa['bairro']    = '';
+    $pessoa['telefone']  = '';
+    $pessoa['email']     = '';
+    $pessoa['id_cidade'] = '';
+}
 
-
+require_once 'lista_combo_cidades.php';
+$form = file_get_contents('html/formulario.html');
+$form = str_replace('{id}',        $pessoa['id'],        $form);
+$form = str_replace('{nome}',      $pessoa['nome'],      $form);
+$form = str_replace('{endereco}',  $pessoa['endereco'],  $form);
+$form = str_replace('{bairro}',    $pessoa['bairro'],    $form);
+$form = str_replace('{telefone}',  $pessoa['telefone'],  $form);
+$form = str_replace('{email}',     $pessoa['email'],     $form);
+$form = str_replace('{id_cidade}', $pessoa['id_cidade'], $form);
+$form = str_replace('{cidades}',   lista_combo_cidades( $pessoa['id_cidade'] ), $form);
+print $form;
 
 ?>
