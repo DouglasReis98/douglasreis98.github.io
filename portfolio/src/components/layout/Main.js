22e450e449254a9a198656f72e9e39e4f89f4884
DouglasReis <douglasamaralreis@outlook.com>
2023-11-11 12:04:40 -0300

    simple-typer

--- portfolio/src/components/layout/Main.js
+++ portfolio/src/components/layout/Main.js
@@ -1,34 +1,18 @@
-import { useState, useEffect} from 'react'
 import styles from './css/Main.module.css'
 import video from './../../videos/video-fundo.mp4'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import imagem from './../../img/douglas_avatar3.png'
 function Main() {
 
-    const TypingOcupacoes = () => {
-        const arrOcups = ["Analista de Sistemas", "Desenvolvedor Full - Stack", "Programador"]
-        const [currentText, setCurrentText] = useState("")
-        const [currentIndex, setCurrentIndex] = useState(0)
+    const arrOcups = ["Analista de Sistemas", "Desenvolvedor Full - Stack", "Programador"]
 
-        useEffect(() => {
-            const intervalId = setInterval(() => {
-                const currentChar = arrOcups[currentIndex][currentText.length];
-                
-                if (currentChar) {
-                    setCurrentText((prevText) => prevText + currentChar)
-                } else {
-                    clearInterval(intervalId)
-                    setTimeout(() => {
-                        setCurrentIndex((prevIndex) => (prevIndex + 1) % arrOcups.length);
-                        setCurrentText('');
-                    }, 2000);
-                }            
-            }, 100);
-
-            return () => clearInterval(intervalId);
-        }, [currentIndex, currentIndex, arrOcups]);
-        
-        return currentText;
-    }
+    const [ocups] = useTypewriter({ 
+        words: arrOcups,
+        typeSpeed: 80,
+        deleteSpeed: 50,
+        delaySpeed: 1500,
+        loop: 0
+    });
 
     return (
         <section className={styles.main}>
@@ -41,7 +25,7 @@ function Main() {
                 <div className={styles.texto}>
                     <h1>Seja bem vindo(a)!<br/> Meu nome é</h1>
                     <h2>DOUGLAS REIS</h2>
-                    <h3><span>{TypingOcupacoes()}</span></h3>
+                    <h3><span>{ocups}</span><Cursor cursorColor='#0038FF'/></h3>
                     <h4>Este é o meu portfólio, onde você poderá apreciar os meus projetos de sites, sistemas e aplicações diversas.</h4>
                 </div>
                 <div className={styles.foto} />       
